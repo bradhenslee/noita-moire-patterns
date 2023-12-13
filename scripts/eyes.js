46fa400cfa7cdbff49428d857c0f9d9c9d08c9e2
Brad Henslee <bradhenslee@gmail.com>
2023-12-12 20:32:50 -0800

    instead add image uploader, not movable yet

--- scripts/eyes.js
+++ scripts/eyes.js
@@ -12,10 +12,19 @@ const eyeData = {
 "e5": ["uuuouldolooouoooooouorudrddurouldolludd", "ddroluoorrrrldururldoldodoouuoruuuurldo", "uourulrrddororloulullrurrrrrdorurrudrdd", "dodluuorrlouroroludoroorrlrlrorlodluror", "ouluuouuluoduuuouorlouuorolouoouduooudo", "ruuruuudouuolluruuuurlodluourroloolurud", "uoroluolurruudludouddoudrldouuolrouorru", "ororodoorrloouourollrduuolruuuulroduuor", "udurrlrrorrrolu", "rdrllruoudduldu"]
 };
 
-const messages =  document.getElementById("messages");
+const messages = document.getElementById("messages");
 const svgCloudsLeft = document.getElementById("svg-cloud-layer-l");
 const svgCloudsRight = document.getElementById("svg-cloud-layer-r");
+const rotatedOverlays = document.getElementById("layer3layer4");
 const diamond = document.getElementById("diamond");
+const imgUploader = document.getElementById("image_uploads");
+const customImageLayer = document.getElementById("custom-image-layer");
+const layerSelector = document.getElementById('change-layer-selection');
+
+const movableLayers = [diamond, svgCloudsLeft, svgCloudsRight, rotatedOverlays, customImageLayer];
+
+let selectedForMovement = 0; 
+
 const altars = document.getElementById("altars");
 const drake = dragula([svgCloudsLeft]).on('drag', function (el) {
     messages.textContent = 'Dragging: ' + el.firstChild.ariaLabel;
@@ -38,32 +47,35 @@ document.addEventListener("keydown", function(e) {
         moveDiv(moveKeys[e.key])}
 });
 function moveDiv(vh) {
-    let el = document.getElementById('diamond');
-    let top = el.offsetTop
-    let left = el.offsetLeft
-    if (vh[0] == -1) el.style.top = top - 1 + 'px'
-    if (vh[0] == 1) el.style.top = top + 1 + 'px'
-    if (vh[1] == -1) el.style.left = left - 1 + 'px'
-    if (vh[1] == 1) el.style.left = left + 1 + 'px'
+    const $movable = movableLayers[selectedForMovement];
+    console.log('movable:',$movable)
+    let top = $movable.offsetTop
+    let left = $movable.offsetLeft
+    if (vh[0] == -1) $movable.style.top = top - 1 + 'px'
+    if (vh[0] == 1) $movable.style.top = top + 1 + 'px'
+    if (vh[1] == -1) $movable.style.left = left - 1 + 'px'
+    if (vh[1] == 1) $movable.style.left = left + 1 + 'px'
 }
 
 // event handling - other controls
 const toggleClass = (parentClass, toggleClass) => document.querySelectorAll(parentClass)
-    .forEach(el => el.classList.toggle(toggleClass));
+.forEach(el => el.classList.toggle(toggleClass));
 
-function changeHandler(propName, newVal) {
-    if (propName === 'saveorder') saveOrder(getCurrentOrder())
-    if (propName === 'addlayer') addLayer() 
-    if (propName === 'addmirroredlayer') addMirroredLayer() 
-    if (propName === 'diamond') diamond.classList.toggle('show')
-    if (propName === 'altars') altars.classList.toggle('show')
-    if (propName === 'trigramorder') toggleClass('.svg-cloud', 'hide-trigram-order')
-    if (propName === 'trigrams') toggleClass('.svg-cloud', 'hide-trigrams')  
-    if (propName === 'cloudnames') toggleClass('.svg-cloud', 'hide-cloudnames')    
-    if (propName === 'eyecolors') toggleClass('.svg-cloud', 'hide-colors')    
+imgUploader.addEventListener("change", updateImageDisplay);
+
+function changeHandler(v, newVal) {
+    if (v == 'saveorder') saveOrder(getCurrentOrder())
+    if (v == 'addlayer') addLayer() 
+    if (v == 'addmirroredlayer') addMirroredLayer() 
+    if (v == 'diamond') diamond.classList.toggle('show')
+    if (v == 'altars') altars.classList.toggle('show')
+    if (v == 'trigramorder') toggleClass('.svg-cloud', 'hide-trigram-order')
+    if (v == 'trigrams') toggleClass('.svg-cloud', 'hide-trigrams')  
+    if (v == 'cloudnames') toggleClass('.svg-cloud', 'hide-cloudnames')    
+    if (v == 'eyecolors') toggleClass('.svg-cloud', 'hide-colors')  
 }
 
-// proceedural - parse eyeData, populating layerDiv with nine "eyeclouds" ordered by displayOrder 
+// procedural - parse eyeData, populating layerDiv with nine "eyeclouds" ordered by displayOrder 
 function buildEyeArrangement(eyeData, displayOrder, layerDiv) {
     let eyeArrangementFrag = document.createDocumentFragment();
     let cloudCount = 0;
@@ -88,7 +100,7 @@ function buildEyeArrangement(eyeData, displayOrder, layerDiv) {
             rowData.split('').map((eyeDirection) => {
                 let eyeWrap = document.createElement("span");
                 eyeWrap.setAttribute('id', `cloud-${rowCount}_row-${rowCount}_eye-${eyeCount}`);
-                eyeWrap.setAttribute('aria-label', convertDirsToInts(eyeDirection));
+                eyeWrap.setAttribute('aria-label', eyeValueMap(eyeDirection));
                 eyeWrap.innerHTML = `<div class="${eyeDirection}"></div><p></p>`
                 rowFrag.appendChild(eyeWrap);
                 eyeCount++;
@@ -103,16 +115,16 @@ function buildEyeArrangement(eyeData, displayOrder, layerDiv) {
         cloudCount++;
     });
     layerDiv.appendChild(eyeArrangementFrag);
-    // TODO: move delgation up, combine listeners
+    // TODO: move delegation up, combine listeners
     layerDiv.querySelectorAll('.svg-cloud').forEach(el => {
         el.addEventListener("mouseup", (e) => rotateCloud(e.currentTarget, e));
     });
-
     messages.innerHTML = `Loaded saved order<br>${getCurrentOrder()}`;
 }
-
-
-
+function changeSelectedLayer(val) {
+    selectedForMovement = val; 
+    messages.innerHTML = `Layer selected: ${selectedForMovement}`;
+}
 // Rotate a cloud 1/4 turn
 function rotateCloud(el) {
     let currentAngle = el.style['-webkit-transform']
@@ -134,7 +146,7 @@ const getCurrentOrder = (eyeLayer) => {
 const saveOrder = () => {
     let order = getCurrentOrder();
     window.localStorage.setItem("eye-orders", order);
-    messages.textContent = `saved order:\n ${order}`;
+    messages.textContent = `Saved order to HTML5 local storage:\n ${order}`;
 }
 
 function addLayer() {
@@ -147,8 +159,8 @@ function addMirroredLayer() {
     let right = svgCloudsRight.cloneNode(true);
     left.id = "svg-cloud-layer-l-m"; 
     right.id = "svg-cloud-layer-r-m";    
-    svgCloudsLeft.parentElement.appendChild(left);
-    svgCloudsLeft.parentElement.appendChild(right);
+    rotatedOverlays.appendChild(left);
+    rotatedOverlays.appendChild(right);
 }
 
 const moveKeys = { "ArrowUp": [-1,0], "ArrowDown": [1,0], "ArrowLeft": [0,-1], "ArrowRight": [0,1] }
@@ -185,8 +197,75 @@ function readEyeData(eyeData) {
     console.table(ret)
     return ret;
 }
-
-const convertDirsToInts = (str) => str.replaceAll('o',0).replaceAll('u',1).replaceAll('r',2).replaceAll('d',3).replaceAll('l',4);
+const eyeValueMap = (eyeVal) => ({'o':0, 'u':1, 'r':2, 'd':3, 'l':4}[eyeVal]);
+const convertDirsToInts = (strArr) => strArr.split('').map(eye =>eyeValueMap(eye)).join('');
 
 buildEyeArrangement(eyeData, displayOrder, svgCloudsLeft);
 readEyeData(eyeData);
+
+
+
+
+function returnFileSize(number) {
+    if (number < 1024) {
+      return `${number} bytes`;
+    } else if (number >= 1024 && number < 1048576) {
+      return `${(number / 1024).toFixed(1)} KB`;
+    } else if (number >= 1048576) {
+      return `${(number / 1048576).toFixed(1)} MB`;
+    }
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+const fileTypes = [ "image/apng", "image/bmp", "image/gif", "image/jpeg", "image/pjpeg", "image/png", "image/svg+xml", "image/tiff", "image/webp", "image/x-icon", ];
+  
+function validFileType(file) {
+return fileTypes.includes(file.type);
+}
+function updateImageDisplay() {
+    while (customImageLayer.firstChild) customImageLayer.removeChild(customImageLayer.firstChild);
+    
+
+    const curFiles = imgUploader.files;
+    if (curFiles.length === 0) {
+        const para = document.createElement("p");
+        para.textContent = "No files currently selected for upload";
+        customImageLayer.appendChild(para);
+    } else {
+        const list = document.createElement("ol");
+        customImageLayer.appendChild(list);
+
+        for (const file of curFiles) {
+        const listItem = document.createElement("li");
+        const para = document.createElement("p");
+        if (validFileType(file)) {
+            para.textContent = `File name ${file.name}, file size ${returnFileSize(
+            file.size,
+            )}.`;
+            const image = document.createElement("img");
+            image.src = URL.createObjectURL(file);
+
+            // image.alt = image.title = file.name;
+
+            listItem.appendChild(image);
+            // listItem.appendChild(para);
+            addMovableImage(customImageLayer);
+            
+        } else {
+            para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
+            listItem.appendChild(para);
+        }
+
+        list.appendChild(listItem);
+    }
+}
+function addMovableImage(customImageLayer) {
+    movableLayers.push(customImageLayer);
+    let l = layerSelector.options.length;
+    layerSelector.options[l] = new Option(`Uploaded Image ${l}`, l);
+    selectedForMovement = l;
+}
+
+}
+        
